b7a8947a1bff3f52ff0271c82a475d6cde7a4ab1
dewaley <adewaleademilua@gmail.com>
2024-01-01 11:28:01 +0100

    Responsive home ui

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,22 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Shazam',
-  description: 'A clone of the shazam app, built with Next JS.',
-}
+  title: "Shazam",
+  description: "A clone of the shazam app, built with Next JS.",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap'
+          rel='stylesheet'
+        />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
